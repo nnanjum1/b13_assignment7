ee81eead8fae08f84f204a442d619f00f10775a0
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-04-15 23:29:24 +0600

    banner done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Banner from './section/Banner'
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className='bg-[#f8fafc]'>
+            <Banner />
+        </div>
     )
 }
 
---
+++ src/pages/section/Banner.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa'
+import { BannerCard } from '../../components/BannerCard'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="hero bg-[#f8fafc] pt-20">
+                <div className="hero-content text-center flex flex-col">
+
+                    <h1 className="text-5xl font-bold text-[#1F2937]">Friends to keep close in your life</h1>
+                    <p className="py-6 font-medium max-w-[563px] text-[#64748b] ">
+                        Your personal shelf of meaningful connections. Browse, tend, and nurture the
+                        relationships that matter most.
+                    </p>
+                    <button className="btn btn-primary bg-[#244d3f] text-semibold"><FaPlus /> Add A Friend</button>
+
+                </div>
+            </div>
+
+            <div className='w-[70%] mx-auto grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 py-10'>
+
+                <div className="card bg-white shadow-sm">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title text-[#1F2937]">10</h2>
+                        <p className='text-[#64748b]'>Total Friends</p>
+                    </div>
+                </div>
+
+                <div className="card bg-white shadow-sm">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title text-[#1F2937]">3</h2>
+                        <p className='text-[#64748b]'>On Track</p>
+                    </div>
+                </div>
+
+                <div className="card bg-white shadow-sm">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title text-[#1F2937]">6</h2>
+                        <p className='text-[#64748b]'>Need Attention</p>
+                    </div>
+                </div>
+
+                <div className="card bg-white shadow-sm">
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title text-[#1F2937]">12</h2>
+                        <p className='text-[#64748b]'>Interactions This Month</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Banner
